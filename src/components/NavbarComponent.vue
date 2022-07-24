de3eb73b92322b2f4303954e5d0c7f6a7850b5ac
sihle <gubug777@gmail.com>
2022-07-24 20:38:06 +0800

    [add] 新增 NavbarComponent

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="flex justify-between py-4 px-6">
+    <h1>
+      <router-link to="/" class="block w-[255px] h-[38.45px] bg-[url('src/assets/images/logo_lg.png')] bg-no-repeat bg-center bg-cover indent-[101%] whitespace-nowrap overflow-hidden">ONLINE TODO LIST</router-link>
+    </h1>
+    <ul class="flex">
+      <li class="font-bold mr-6">王小明的待辦</li>
+      <li><a href="#">登出</a></li>
+    </ul>
+  </nav>
+</template>
