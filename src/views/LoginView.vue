fc4f14b11d96ef3ec0d765a68d030e9393d860cc
sihle <gubug777@gmail.com>
2022-07-24 17:23:35 +0800

    [add] 新增 LoginView.vue

---
+++ src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container h-screen mx-auto">
+    <div class="h-full flex justify-center items-center">
+      <div class="grid grid-cols-12 gap-x-4">
+        <div class="hidden md:block col-start-2 col-span-5">
+          <img class="w-full" src="../assets/images/left.png" alt="Hero">
+        </div>
+        <div class="col-span-12 md:col-start-8 md:col-span-4">
+          <form @submit.prevent="" class="h-full flex flex-col justify-center text-[#333]">
+            <img class="block sm:hidden mb-4" src="../assets/images/logo_lg.png" alt="Logo">
+            <h1 class="text-center md:text-left font-bold text-2xl mb-8 md:mb-6">最實用的線上待辦事項服務</h1>
+            <div class="mb-4">
+              <label class="block text-[14px] font-bold mb-1" for="email">Email</label>
+              <input class="block w-full rounded-[10px] placeholder-shown:text-[#9F9A91] focus-visible:outline-amber-700 py-3 px-4" id="email"
+                type="text" placeholder="請輸入Email">
+            </div>
+            <div class="mb-6">
+              <label class="block text-[14px] font-bold mb-1" for="password">密碼</label>
+              <input class="block w-full rounded-[10px] placeholder-shown:text-[#9F9A91] focus-visible:outline-amber-700 py-3 px-4" id="password"
+                type="password" placeholder="請輸入密碼">
+            </div>
+            <input class="block cursor-pointer rounded-[10px] text-white hover:text-gray-300 bg-[#333] py-3 px-12 | mx-auto mb-6" type="submit" value="登入">
+            <div class="text-center">
+              <a class="" href="#">註冊帳號</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    找不到該頁面
+  </div>
+</template>
